4b59821e774a9cf6d4e7e25cf9971bfec98baa1b
szl156 <1723672621@qq.com>
2023-02-01 15:07:02 +0800

    BusinessCard Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,33 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/index.module.scss'
-import { NextPage } from 'next'
+import {GetServerSideProps, NextPage} from 'next'
+import BusinessCard, {BusinessCardData} from '@/components/BusinessCard/BusinessCard'
+import axios from "axios";
 
-const Home: NextPage = () => {
-  return (
-    <>
-      <div className={styles.test}>首页</div>
-      <div className={styles.test}>首页</div>
-      <div className={styles.test}>首页</div>
-      <div className={styles.test}>首页</div>
-      <div className={styles.test}>首页</div>
-      <div className={styles.test}>首页</div>
-      <div className={styles.test}>首页</div>
-    </>
-  )
+export interface IHomeProps {
+    BusinessCardData: BusinessCardData[]
 }
+
+const Home: NextPage<IHomeProps> = (BusinessCardData) => {
+    return (
+        <>
+            <BusinessCard {...BusinessCardData}/>
+            <div className={styles.test}>首页</div>
+            <div className={styles.test}>首页</div>
+            <div className={styles.test}>首页</div>
+            <div className={styles.test}>首页</div>
+            <div className={styles.test}>首页</div>
+            <div className={styles.test}>首页</div>
+            <div className={styles.test}>首页</div>
+        </>
+    )
+}
+export const getServerSideProps: GetServerSideProps = async context => {
+    const res = await axios.get("http://localhost:3000/api/BusinessCardData")
+    return {
+        props: res.data
+    }
+}
+
 export default Home
