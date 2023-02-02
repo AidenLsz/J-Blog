235f9fb2eb11165b68b1b4201f634d0df68fea96
lzzt <1723194885@qq.com>
2023-02-02 23:04:51 +0800

    fix: 相关文章组件

---
+++ components/RelatedArticles/RelatedArticles.tsx
@@ -0,0 +1,28 @@
+
+import styles from './RelatedArticles.module.css'
+import Link from 'next/link'
+
+const RelatedArticles=({article}:any)=>{
+
+    return(
+    <div className={styles.block}>
+        <div className={styles.block_title}>相关文章</div>
+        <div className={styles.block_body}>
+            <div className={styles.block_list}>
+                {article&&article.map((item:any,index:number)=>{
+                  if(index<10){
+                    return (<Link className={styles.item} href="#" key={item.id}><div className={styles.entry_title} >{item.attributes.title}</div></Link>)
+                  }
+                  else{
+                    return
+                  }
+                })}
+                
+            </div>
+        </div>
+    </div>
+    )
+}
+  
+  //根据前一个页面传过来的tab，找出对应文章的前十个
+export default RelatedArticles
