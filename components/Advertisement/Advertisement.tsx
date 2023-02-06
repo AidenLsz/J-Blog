1b328d15ab81f01f479090e66baf4421731790b2
szl156 <1723672621@qq.com>
2023-02-06 14:57:46 +0800

    refactor: ts类型约束 对项目组件中的ts文件补充ts类型约束，同时进行代码合并

--- components/Advertisement/Advertisement.tsx
+++ components/Advertisement/Advertisement.tsx
@@ -18,13 +18,13 @@ export interface AdvertisementItemProps {
 }
 
 export interface AdvertisementProps {
-  AdvertisementData: AdvertisementItemProps[]
+    AdvertisementData: AdvertisementItemProps
 }
 
-const Advertisement: NextPage<AdvertisementProps> = ({ AdvertisementData }) => {
-  function filterDownload(AdvertisementData: AdvertisementItemProps[]) {
-    return AdvertisementData.filter((item) => item.attributes.title)
-  }
+const Advertisement: NextPage<AdvertisementProps> = ({AdvertisementData}) => {
+    function filterDownload(AdvertisementData: AdvertisementItemProps) {
+        return Object.values(AdvertisementData).filter((item) => item.attributes.title)
+    }
 
   const downloadData = filterDownload(AdvertisementData)
 
@@ -36,7 +36,7 @@ const Advertisement: NextPage<AdvertisementProps> = ({ AdvertisementData }) => {
           alt="广告"
           width={400}
           height={400}
-        ></Image>
+                />
         <div className={style["advertisement-label"]}>
           <span>广告</span>
         </div>
@@ -48,7 +48,7 @@ const Advertisement: NextPage<AdvertisementProps> = ({ AdvertisementData }) => {
           alt="二维码"
           width={500}
           height={500}
-        ></Image>
+                />
         <div className={style["download-info"]}>
           <div className={style["download-headline"]}>
             {downloadData[0].attributes.title}
