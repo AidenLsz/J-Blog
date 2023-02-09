398eeb9e27e314ef27d2ef253789d8e6488bb109
AidenLsz <aiden_lsz@mail.ustc.edu.cn>
2023-02-09 16:06:24 +0800

    fix: 修改新增文章功能异常

--- components/timeline_entrylist/advertisement.tsx
+++ components/timeline_entrylist/advertisement.tsx
@@ -37,12 +37,12 @@ function Advertisement({ data, article_tab,handlerLoading }: any): JSX.Element {
   });
 
   if (article_tab == 1) {
-    data.sort((a, b) => {
+    articles.sort((a, b) => {
       return a.id - b.id
     })
   }
   if (article_tab == 2) {
-    data.sort(function (a, b) {
+    articles.sort(function (a, b) {
       if (a.attributes.updatedAt > b.attributes.updatedAt) {
         return -1
       } else {
@@ -51,7 +51,7 @@ function Advertisement({ data, article_tab,handlerLoading }: any): JSX.Element {
     })
   }
   if (article_tab == 3) {
-    data.sort((a, b) => {
+    articles.sort((a, b) => {
       if (a.attributes.view_count > b.attributes.view_count) {
         return -1
       } else {
@@ -78,8 +78,47 @@ function Advertisement({ data, article_tab,handlerLoading }: any): JSX.Element {
                 </a>
               </div>
               <div className={Styles.dividing}></div>
-              <div className={Styles.date}>{post.attributes.date}</div>    
+              <div className={Styles.date}>{post.attributes.date}</div>  
+              <div className={`${post.attributes.ad?Styles.none:Styles.tag_list}`}>
+      {(`${post.attributes.article_type_tabs.data}`.length==0 &&
+      `${post.attributes.tags.data}`.length==0)?
+         null
+        :
+        <div className={`${post.attributes.ad?Styles.none:Styles.dividing_left}`}></div>}
+
+        {`${post.attributes.article_type_tabs.data}`.length==0 ||
+        `${post.attributes.tags.data}`.length!=0?
+        null
+        :
+        post.attributes.article_type_tabs.data.map((tab:any)=>
+        <a key={tab.id} className={Styles.tag}>
+          {tab.attributes.title}</a>
+        )}
+        {`${post.attributes.tags.data}`=="null"?
+        null
+        :
+        post.attributes.tags.data.map((tag:any)=>
+        <a key={tag.id} className={Styles.tag}>
+          {tag.attributes.title}</a>
+        )}                
+        </div>
+        <div className={`${post.attributes.ad?Styles.is_ad:Styles.not_ad}`}>广告</div>
+        <div className={`${post.attributes.ad?Styles.not_article:Styles.is_article}` } 
+        onClick={()=>{
+          setDislike([
+            ...dislike,post.id
+          ]
+          )
+        }}>
+<Image 
+ width={15}
+ height={15}
+ alt="X"
+ src="/images/cross.svg"
+ />
+     </div>  
             </div>
+            
             <div className={Styles.main}>
               <div className={Styles.info_box}>
                 <a className={Styles.title}>{post.attributes.title}</a>
