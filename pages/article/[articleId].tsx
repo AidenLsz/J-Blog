3b49b0b8f651f5595e8cf43f43773bf3e3ff9e20
szl156 <1723672621@qq.com>
2023-02-11 18:26:13 +0800

    refactor: 文章详情页动态个人名片 完善个人名片组件，同时加入BFF完成动态更新

--- pages/article/[articleId].tsx
+++ pages/article/[articleId].tsx
@@ -1,15 +1,14 @@
-import {GetServerSideProps, NextPage} from 'next'
-import {LOCALDOMAIN, SERVERDOMAIN} from 'utils'
+import { NextPage, GetServerSideProps } from 'next'
+import { LOCALDOMAIN,SERVERDOMAIN } from 'utils'
 import Image from 'next/image'
-import {Converter} from 'showdown'
+import { Converter } from 'showdown'
 import styles from './Article.module.scss'
 import axios from 'axios';
 import MarkNav from 'markdown-navbar';
 import 'markdown-navbar/dist/navbar.css';
-import RelatedArticles, {article} from '@/components/RelatedArticles/RelatedArticles';
-import BusinessCard, {BusinessCardData} from '@/components/BusinessCard/BusinessCard'
-import {IArticleProps} from '../api/ArticleInfo'
-
+import RelatedArticles, { article } from '@/components/RelatedArticles/RelatedArticles';
+import BusinessCard, { BusinessCardData } from '@/components/BusinessCard/BusinessCard'
+import { IArticleProps } from '../api/ArticleInfo'
 interface dataProps {
     currentArticle: IArticleProps,
     relatedArticles: article[],
@@ -26,7 +25,6 @@ const ArticleDetail: NextPage<dataProps> = ({
         simplifiedAutoLink: true,
         emoji: true,
     })
-    // console.log('carddata',business.job_title)
     return (
         <div className={styles.contain}>
             <div className={styles.content}>
@@ -35,35 +33,32 @@ const ArticleDetail: NextPage<dataProps> = ({
                     作者：{article_detail.data.attributes.AuthorName} | 创建时间: {createdAt}
                 </div>
                 {/* <div className={styles.description}>{description}</div> */}
-                {image.data &&
-                    <Image src={SERVERDOMAIN + image.data[0].attributes.url} alt='文章封面' width={700} height={400}/>}
-                <article
-                    dangerouslySetInnerHTML={{__html: converter.makeHtml(article_detail.data.attributes.description)}}
-                    className={styles['markdown-body']}/>
+                {image.data && <Image src={SERVERDOMAIN + image.data[0].attributes.url} alt='文章封面' width={700} height={400} className={styles.article_img} />}
+                <article dangerouslySetInnerHTML={{ __html: converter.makeHtml(article_detail.data.attributes.description) }} className={styles['markdown-body']} />
             </div>
             <div className={styles.sideCard}>
-                <BusinessCard BusinessCardData={business}/>
-                <RelatedArticles article={relatedArticles}/>
-                <div style={{backgroundColor: 'white', paddingTop: '10px'}}>
-                    <div className={styles.navTitle}>文章目录</div>
+                <BusinessCard BusinessCardData={business} />
+                <RelatedArticles article={relatedArticles} />
+                <div className={styles.article_menu}>
+                <div className={styles.navTitle}>文章目录</div>
                     <MarkNav
-                        className="article-menu"
+                        className={styles.article_menu}
                         source={article_detail.data.attributes.description}
                         headingTopOffset={0}
                         ordered={false}
                     />
                 </div>
             </div>
-        </div>
-    )
+            </div>
+            )
 }
 
 
 export const getServerSideProps: GetServerSideProps = async context => {
     const {articleId} = context.query;
-    const {data: currentArticle} = await axios.get(`${LOCALDOMAIN}/api/ArticleInfo`, { //获取当前文章
+    const {data:currentArticle } = await axios.get(`${LOCALDOMAIN}/api/ArticleInfo`, { //获取当前文章
         params: {
-            articleId,
+        articleId,
         },
     });
     let business = await axios.get(`${LOCALDOMAIN}/api/BusinessCardData`, { //获取作者信息
@@ -73,7 +68,6 @@ export const getServerSideProps: GetServerSideProps = async context => {
     })
     business = business.data
     console.log('business.data', business)
-    // console.log(currentArticle)
     const relatedArticles = await getRelatedArticles(articleId)
     return {
         props: {relatedArticles, currentArticle, business}, // 需要拿props包裹
